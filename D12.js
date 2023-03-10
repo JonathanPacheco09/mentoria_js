4f44ca5683d31e8f166ba4d0d29a508c9ca4365a
jonathan <fjonathanrpacheco09@gmail.com>
2023-03-10 19:52:29 -0300

    desafio1012

---
+++ D12.js
@@ -0,0 +1,25 @@
+//calculo de area de um triangulo retangulo 
+var A = 5 
+var C = 4
+var Area = A * C / 2
+console.log(`a area do triangulo retangulo é ${Area}`)
+//área do círculo de raio
+var P = 3.14
+var C = 5
+var Area = P * (C*C)
+console.log(`a area do circulo é ${Area}`)
+//area do trapezio 
+var A = 30
+var b = 22
+var H = 25
+var Area = (A+b)*H/2 
+console.log(`a area do trapezio é ${Area}`)
+//area do quadrado
+var B = 10
+var Area = B*B 
+console.log(`a area do quadrado é ${Area}`)
+//area do retangulo
+var A = 10
+var B = 20
+var Area = A*B 
+console.log(`a area do retangulo é ${Area}`)
--- desafio_1002.js
+++
@@ -1,4 +0,0 @@
-var n = 3.14159
-var raio = 2.00
-var area = n * (raio * raio)
-console.log(area)
